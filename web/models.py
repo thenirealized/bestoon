7f2ff5b66a3dfa94a8130e7a0f62171a7d14f830
hadihaddad <hadi_63h@yahoo.com>
2018-08-02 13:53:22 +0200

    income in models and some beautification

--- web/models.py
+++ web/models.py
@@ -7,4 +7,14 @@ class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user =  models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
